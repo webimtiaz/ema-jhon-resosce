b823aeaf5e1c4bcb73b838be143d0bb64b6d346a
imtiaz <imtiazonindo@gmail.com>
2023-03-28 23:08:58 +0600

    first commit

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
-    console.log(props)
+    console.log(props);
     
     const {id, name, seller,ratings, quantity, price, img}=props.product;
     const hendelaradded = props.hendelaradded
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Card from '../Card/Card';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -11,7 +11,29 @@ const Shop = () => {
         fetch('products.json')
         .then(res => res.json())
         .then(data =>setProducts(data));
+
+       
     },[])
+    useEffect(()=>{
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+    for(const id in storedCart){
+        const addedProduct = products.find(product => product.id === id);
+    
+        if(addedProduct){
+            // step 3: add quantity
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            // step 4: add the added product to the saved cart
+            savedCart.push(addedProduct);
+        }
+        // const quantity = storedCart[id];
+        // addedProduct.quantity = quantity;
+        
+        console.log(addedProduct);
+    }
+    setCard(savedCart);
+    },[products])
 
     const hendelaradded = (product) =>{
        const newcard = [...card, product];
